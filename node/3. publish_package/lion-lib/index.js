2bef12d160e722e20b26b987594daa97314ad101
Shashwat-Joshi <shashwat2002joshi@gmail.com>
2021-04-24 13:14:20 +0530

    basics for npm

---
+++ node/3. publish_package/lion-lib/index.js
@@ -0,0 +1,9 @@
+export default class Operations {
+  add(a, b) {
+    return a + b;
+  }
+
+  sub(a, b) {
+    return a - b;
+  }
+}
---
+++ node/3. publish_package/nodeapp/index.js
@@ -0,0 +1,7 @@
+import Operations from "shashwat1234";
+const operations = new Operations();
+
+const res1 = operations.add(1, 4);
+const res2 = operations.sub(8, 4);
+
+console.log(res1, res2);
