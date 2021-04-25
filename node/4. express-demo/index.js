6496611910323946bd726d61ceeef88697b75a3c
Shashwat-Joshi <shashwat2002joshi@gmail.com>
2021-04-25 18:21:33 +0530

    basics of express

--- node/4. express-demo/index.js
+++ node/4. express-demo/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 const app = express();
+import Joi from "joi";
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "course1" },
@@ -31,6 +34,65 @@ app.get("/api/courses/:id", (req, res) => {
   } else {
     res.send(course);
   }
+});
+
+app.post("/api/courses", (req, res) => {
+  // const result = validateCourse(req.body);
+  const { error } = validateCourse(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  // Lookup the course if not exists return 404
+  const course = courses.find((course) => {
+    return course.id === parseInt(req.params.id);
+  });
+  if (!course) return res.status(404).send("Course not found with given id");
+
+  // Validate
+  const { error } = validateCourse(req.body);
+
+  // if not valid return 400
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  // Update and return course
+  course.name = req.body.name;
+  res.send(course);
+});
+
+// Validation logic
+function validateCourse(courseObj) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(courseObj);
+}
+
+app.delete("/api/courses/:id", (req, res) => {
+  // Lookup the course if not exists return 404
+  const course = courses.find((course) => {
+    return course.id === parseInt(req.params.id);
+  });
+  if (!course) return res.status(404).send("Course not found with given id");
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // Return the response
+  res.send(course);
 });
 
 // Imp: In production the port can be dynamically assigned
